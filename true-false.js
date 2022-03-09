8b0a2f5345a01c66f35fdae900e4a6cddfe9face
orfrohan <soikotrohan08@gmail.com>
2022-03-09 15:29:40 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// Falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+let name = false;
+
+if (name) {
+  console.log("condition is true");
+} else {
+  console.log("condition is false");
+}
